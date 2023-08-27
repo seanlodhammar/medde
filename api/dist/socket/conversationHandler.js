634b105f00ea5e8fe2de6eb42bf1e7f50c3652cc
Sean Lodhammar <88111973+seanlodhammar@users.noreply.github.com>
2023-08-27 17:50:23 +0100

    Deleted /dist directory and added to .gitignore

--- api/dist/socket/conversationHandler.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const conversationHandler = (io) => {
-    const joinConversation = () => {
-        console.log('testing');
-    };
-    return { joinConversation };
-};
-exports.default = conversationHandler;
--- api/dist/socket/customerHandler.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const customerHandler = (io, socket) => {
-    socket.on('something', () => {
-        console.log('wow');
-    });
-};
-exports.default = customerHandler;
